e5fcbe104a614c2d2292dfe82d25f0c7be2e8b61
Marcin Ciesielski <marcin.ciesielski@pw.edu.pl>
2019-03-16 14:44:35 +0100

    Create first model

--- twitter/models.py
+++ twitter/models.py
@@ -1,3 +1,16 @@
+import datetime
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+TWITTER_MAXIMUM_TWEET_LENGTH = 280
+
+
+class Tweet(models.Model):
+    content = models.CharField(max_length=TWITTER_MAXIMUM_TWEET_LENGTH)
+    creation_date = models.DateField(default=datetime.date.today)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'[{self.creation_date}] ' \
+            f'TWEET by {self.author}: {self.content[:20]}'

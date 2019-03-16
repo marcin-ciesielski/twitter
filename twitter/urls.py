55b9faace40dc70c28e755fa92010debee1feb5b
Marcin Ciesielski <marcin.ciesielski@pw.edu.pl>
2019-03-16 15:58:48 +0100

    Modify auth, login, logout, password_reset, register

--- twitter/urls.py
+++ twitter/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from django.views import generic
 
-from twitter.views import AllTwiteerView
+from twitter import views
 
 app_name = 'twitter'
 urlpatterns = [
     # path('', generic.TemplateView.as_view(template_name='twitter/index.html'),
     #      name='index'),
-    path('', AllTwiteerView.as_view(), name='index')
+    path('', views.AllTwiteerView.as_view(), name='index'),
+    path('register/', views.RegisterView.as_view(),name='register')
 ]
--- twitter/views.py
+++ twitter/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
 from django.views.generic import ListView
 
+from twitter import forms
 from twitter.models import Tweet
 
 
@@ -12,3 +14,10 @@ class AllTwiteerView(ListView):
 
     def get_queryset(self):
         return Tweet.objects.all().order_by('creation_date')
+
+
+class RegisterView(View):
+
+    def get(self, request):
+        return render(request, 'twitter/register.html',
+                      {'form': forms.UserRegisterForm()})

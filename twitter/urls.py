9f8bca3c9d2d9e79b6f7088add8647bec4fae85d
Marcin Ciesielski <marcin.ciesielski@pw.edu.pl>
2019-03-16 15:40:23 +0100

    Auth

--- twitter/urls.py
+++ twitter/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from django.views import generic
 
+from twitter.views import AllTwiteerView
+
 app_name = 'twitter'
 urlpatterns = [
-    path('', generic.TemplateView.as_view(template_name='twitter/index.html'),
-         name='index'),
+    # path('', generic.TemplateView.as_view(template_name='twitter/index.html'),
+    #      name='index'),
+    path('', AllTwiteerView.as_view(), name='index')
 ]
--- twitter/views.py
+++ twitter/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+
+from twitter.models import Tweet
+
+
+class AllTwiteerView(ListView):
+    template_name = 'twitter/index.html'
+    context_object_name = 'tweets'
+
+    def get_queryset(self):
+        return Tweet.objects.all().order_by('creation_date')

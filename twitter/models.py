4b685f67aa2f395f2ebe09955b137fd765855243
Marcin Ciesielski <marcin.ciesielski@pw.edu.pl>
2019-03-17 14:07:03 +0100

    New Model

--- twitter/models.py
+++ twitter/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 TWITTER_MAXIMUM_TWEET_LENGTH = 280
-
+COMMENT_MAXIMUM_LENGTH = 30
 
 class Tweet(models.Model):
     content = models.CharField(max_length=TWITTER_MAXIMUM_TWEET_LENGTH)
@@ -14,3 +14,13 @@ class Tweet(models.Model):
     def __str__(self):
         return f'[{self.creation_date}] ' \
             f'TWEET by {self.author}: {self.content[:20]}'
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=COMMENT_MAXIMUM_LENGTH)
+    creation_date = models.DateTimeField(default=datetime.date.today)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
